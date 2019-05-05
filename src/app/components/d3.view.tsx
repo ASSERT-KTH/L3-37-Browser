e2c7d7e74c75a81d8420a54c9ccfa2051d9894c2
Javier Cabrera Arteaga <xppcoder@gmail.com>
2019-05-05 15:59:41 +0200

    Declarative way

--- src/app/components/d3.view.tsx
+++ src/app/components/d3.view.tsx
@@ -1,7 +1,8 @@
 
 import * as React from 'react';
 import * as d3 from 'd3';
-
+import Node from './d3/node';
+import Link from './d3/link';
 
 interface IProps{
     width?: number;
@@ -43,38 +44,28 @@ export default class D3View extends React.Component<IProps, any>{
 
     }
 
-    updateLinks = (root) => {
-        console.log("Updating links", root.links())
-        let links = d3.select(this.c).select(".links").selectAll('path.link')
-            .data(root.links());
-        links.exit().remove();
-        links.enter().append('path').classed('link', true).attr('d', this.linkPath);
-        links.transition().attr('d', this.linkPath);
-    }
-
     linkPath = d3.linkRadial().angle( (d: any) => d.x ).radius((d: any) => d.y);
-    nodePosition = (d) => `translate(${d3.pointRadial(d.x, d.y)})`;
+    nodePosition = (d) => {
+        return `translate(${d3.pointRadial(d.x, d.y)})`;
+    }
     svg: any;
 
-    setData(data){
+    setData = async (data) => {
 
         const width = this.props.width || 300;
         const height = this.props.height || 300;
         const MARGIN = 10;
 
         
-        this.setState({
-            data: d
-        })
-        d3.hierarchy(data);
-
-        
+        let root = d3.hierarchy(data)
         const layout = d3.cluster().size([2 * Math.PI, Math.min(width, height)/2 - 2*MARGIN]);
         layout(root);
 
-
-        this.updateNodes(root);
-        this.updateLinks(root);
+        this.setState({
+            data: root
+        })
+        //this.updateNodes(root);
+        //this.updateLinks(root);
     }
 
     drawChart() {
@@ -104,20 +95,51 @@ export default class D3View extends React.Component<IProps, any>{
         this.setData(this.props.data);
     }
 
-    componentWillReceiveProps(nextProps: IProps){
+    componentDidUpdate(){
+        //console.log("Update");
+
+    }
+
+    /*componentWillReceiveProps(nextProps: IProps){
 
         if(nextProps.url !== this.props.url || nextProps.width !== this.props.width || nextProps.height !== this.props.height)
         {
-            this.setData(nextProps.data);
+            console.log("Rerendering")
+            //this.setData(nextProps.data);
 
-            this.forceUpdate();
+            //this.forceUpdate();
         }
-    }
+    }*/
 
     render(){
         return (<svg className='canvas' ref={e => this.c = e}>
-                    <g className="links"></g>
-                    <g className="nodes"></g>
+                    <g className="links">
+                        {
+                            this.state.data && 
+
+                            this.state.data.links().map((l, index) => {
+                                return <Link
+                                    key={index + 'index'}
+                                    path={this.linkPath(l)}
+                                    className='link'
+                                />
+                            })
+                        }
+                    </g>
+                    <g className="nodes">
+                    {this.state.data &&
+                         this.state.data.descendants().map((n, index) => {
+
+                             const coord =d3.pointRadial(n.x, n.y);
+
+                             return (<Node 
+                                key={index + 'node'}
+                                className={'node'}
+                                x={coord[0]}
+                                y={coord[1]}/>)
+                         })
+                        }
+                    </g>
             </svg>);
     }
 }
