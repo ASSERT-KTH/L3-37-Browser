1356b50971f79f70d6401762e1bad9a6f98f50fe
Javier Cabrera Arteaga <xppcoder@gmail.com>
2019-05-05 12:09:58 +0200

    Borwser bar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Layout, Menu, Breadcrumb, Form, Input, Icon } from 'antd';
+import { Layout, Menu, Breadcrumb, Form, Input, Icon, Affix } from 'antd';
 
 const { Header, Content, Footer } = Layout;
 
@@ -16,13 +16,33 @@ class App extends React.Component<any, any>{
   componentDidMount(){
     console.log(document.getElementById('view'))
     if(this.r){
-      /*this.r.addEventListener('did-finish-load', (e) => {
+      this.r.addEventListener('did-finish-load', (e) => {
         console.log(e, this.r.src)
 
-        if(this.r.src != this.state.url){
-          this.setState({url: this.r.src})
+        if(this.r.src !== this.state.url){
+
+          const {
+            setFieldsValue
+          } = this.props.form;
+
+          setFieldsValue({
+            url: this.r.src
+          })
         }
-      });*/
+      });
+    }
+  }
+
+  parseUrl = (val) => {
+    let https = val.slice(0, 8).toLowerCase();
+    let http = val.slice(0, 7).toLowerCase();
+    if (https === 'https://') {
+      return val;
+    } else if (http === 'http://') {
+      return val;
+    } else {
+
+      return 'http://' + val;
     }
   }
 
@@ -30,17 +50,9 @@ class App extends React.Component<any, any>{
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        let val = values.url;
-        let https = val.slice(0, 8).toLowerCase();
-        let http = val.slice(0, 7).toLowerCase();
-        if (https === 'https://') {
-            this.setState({url: val})
-        } else if (http === 'http://') {
-          this.setState({url: val})
-        } else {
+        const val = this.parseUrl(values.url);
 
-          this.setState({url: 'http://' + val})
-        }
+        this.setState({url: val})
       }
     });
   }
@@ -59,12 +71,12 @@ class App extends React.Component<any, any>{
       getFieldDecorator, getFieldsError, getFieldError, isFieldTouched,
     } = this.props.form;
 
-    return (<Form style={{width: '100%', margin: '10px'}} layout="inline" onSubmit={this.handleSubmit}>
+    return (<Form style={{width: '100%', margin: ''}} onSubmit={this.handleSubmit}>
               <Form.Item
               >
                  {getFieldDecorator('url', {
                   })(
-                    <Input value={this.getValue()} prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Url" />
+                    <Input prefix={<Icon type="cloud" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Url" addonAfter={<Icon type="setting" />} />
                   )}
             </Form.Item>
               
@@ -73,11 +85,11 @@ class App extends React.Component<any, any>{
 
   render(){
     return ( <Layout style={{width:'100%', height: '100%'}}>
-                <Header style={{ position: 'fixed', zIndex: 1, width: '100%', height: '60px' }}>
+                <Header style={{ position: 'fixed', zIndex: 1, width: '100%', height: '50px', padding:'5px' }}>
                   
                   {this.renderBar()}
                 </Header>
-                <webview ref={e => this.r = e} style={{width:'100%', height: '100%', paddingTop: '60px'}}  src={this.state.url} />
+                <webview ref={e => this.r = e} style={{width:'100%', height: '100%', paddingTop: '48px'}}  src={this.state.url} />
                 
               </Layout>);
   }
