28f525271d4afd471b22d5d6c37fb6016ce2e393
oscarlvp <oscarlvp@gmail.com>
2019-05-14 13:40:44 +0200

    Removed blending mode control

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,12 +8,7 @@ const TabPane = Tabs.TabPane;
 
 const { Header, Content, Footer, Sider } = Layout;
 
-interface AppState {
-  blendingModeEnabled: boolean;
-}
-
 interface IState{
-  app: AppState;
   url: string,
   collapsed: boolean,
   validUrl: string
@@ -31,9 +26,6 @@ class App extends React.Component<any, IState>{
       collapsed: false,
       validUrl: '',
       opacity: 0.5,
-      app: {
-        blendingModeEnabled: true
-      }
     }
   }
 
@@ -93,61 +85,41 @@ class App extends React.Component<any, IState>{
     return '';
   }
 
-  renderBar(){
-
-    const {
-      getFieldDecorator, getFieldsError, getFieldError, isFieldTouched,
-    } = this.props.form;
-
-    // addonAfter={<Icon type="setting" />} 
-    return (<Form  onSubmit={this.handleSubmit}>
-               
-                  <Form.Item
-                    >
-                      {getFieldDecorator('url', {
-                        })(
-                          <Input prefix={<Icon type="cloud" style={{ color: 'rgba(0,0,0,.25)'}} />} placeholder="Url"/>
-                        )}
-                  </Form.Item>
-            </Form>)
-  }
-
-  getBlendingTab = () => {
-    return (
-      <Slider
-              min={0}
-              max={1}
-              step={0.1}
-              onChange={e => this.setState({opacity: e})}
-              value={this.state.opacity}
-              style={{minWidth: 100}}
-            />)
-  }
-
   render(){
 
     const {
-      getFieldValue
+      getFieldDecorator
     } = this.props.form;
 
     return ( <Layout style={{width:'100%', height: '100%'}}>
-              
                 <Header style={{ position: 'absolute', zIndex: 1, width: '80%', height: '50px', padding:'10px' }}>
-                  
-                  {this.renderBar()}
+                <Form  onSubmit={this.handleSubmit}>
+                  <Form.Item>
+                    {
+                      getFieldDecorator('url', {})(
+                        <Input prefix={<Icon type="cloud" style={{ color: 'rgba(0,0,0,.25)'}} />} placeholder="Url"/>
+                      )
+                    }
+                  </Form.Item>
+                </Form>
                 </Header>
-
                 <div className="card-container">
                   <Tabs tabPosition='top' className='tabs-container' type="card">
-                    <TabPane className={`main-container ${this.state.app.blendingModeEnabled? 'blending': ''}`} forceRender tab={this.state.app.blendingModeEnabled? this.getBlendingTab(): 'Browser'} key="1">
-                          {this.state.app.blendingModeEnabled && <TreeView style={{opacity: 1 - this.state.opacity}} url={this.state.validUrl} />}
+                    <TabPane className='main-container blending' forceRender key="1" 
+                      tab={
+                        <Slider 
+                          min={0} 
+                          max={1} 
+                          step={0.1} 
+                          onChange={e => this.setState({opacity: e})} 
+                          value={this.state.opacity} 
+                          style={{minWidth: 100}
+                        }/>}>
+                          <TreeView style={{opacity: 1 - this.state.opacity}} url={this.state.validUrl} />
                           <webview ref={e => this.r = e} style={{width:'100%', height: '100%', opacity: this.state.opacity}}  src={this.state.url} />
-                    
                     </TabPane>
-                    { !this.state.app.blendingModeEnabled && <TabPane forceRender tab="Art" key="2"><TreeView url={this.state.validUrl} /></TabPane>}
                   </Tabs>
                 </div>
-                
               </Layout>);
   }
 }
