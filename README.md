f65dc674dfbbf9d5a9a76eb795dad6917902d758
Jonathan <ramjonathan@gmail.com>
2020-02-21 11:16:00 +0100

    windows configuration

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # Mini browser through the looking glass [![Build Status](https://travis-ci.org/Jacarte/L3-37-Browser.svg?branch=master)](https://travis-ci.org/Jacarte/L3-37-Browser)
 
-L3-37 is a mini web browser that superimposes a view of the structure of the web page on top of the actual page. Our goal is to reveal and embody the organization and dynamics of software that runs the world wide web. Citizens interact with browsers worldwide, every day and we which to offer a looking through the glass of web content.
+L3-37 is a mini web browser that superimposes a view of the strucuture of the web page on top of the actual page. Our goal is to reveal and embody the organization and dynamics of software that runs the world wide web. Citizens interact with browsers worldwide, every day and we which to offer a looking through the glass of web content.
 
 This browser is developed as a tribute to the [Web stalker](http://bak.spc.org/iod/nettime.html), 
 
@@ -13,10 +13,7 @@ Input the url and press enter
 
 ```
 npm run start
-# if MacOS or linux
 npm run electron-dev
-# else
-npm run electron-win
 ```
 
 ## Developing tips
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,22 +6,22 @@ const TabPane = Tabs.TabPane;
 
 const { Header } = Layout;
 
-interface IState{
-  url :string,
-  isURLValid :boolean
-  collapsed :boolean,
-  opacity :any;
+interface IState {
+  url: string,
+  isURLValid: boolean
+  collapsed: boolean,
+  opacity: any;
 }
 
 class App extends React.Component<any, IState>{
 
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {
       url: 'http://google.com',
       isURLValid: true,
-      collapsed: false,      
+      collapsed: false,
       opacity: 0.5,
     }
 
@@ -38,39 +38,39 @@ class App extends React.Component<any, IState>{
       url
     })
 
-    this.setState({ url})
+    this.setState({ url })
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log(document.getElementById('view'))
-    if(this.pageView){
+    if (this.pageView) {
       //this.pageView.addEventListener('did-start-loading', this.updateUrl);
       this.pageView.addEventListener('did-finish-load', this.updateUrl);
     }
   }
 
-  getURL = (value :string) : URL => {
+  getURL = (value: string): URL => {
     if (!/https?:\/\//.test(value)) {
       value = 'http://' + value;
     }
-    return new URL(value);    
+    return new URL(value);
   }
 
   handleSubmit = (evt) => {
     evt.preventDefault();
     this.props.form.validateFields((err, values) => {
       console.log("validating the parameters");
-      if(err) {
+      if (err) {
         return;
       }
       try {
         let url = this.getURL(values.url);
-        this.setState({url: url.toString(), isURLValid: true});
+        this.setState({ url: url.toString(), isURLValid: true });
 
         this.pageView.src = url;
       }
-      catch(exc) {
-        this.setState({isURLValid: false});
+      catch (exc) {
+        this.setState({ isURLValid: false });
       }
     });
   }
@@ -78,7 +78,7 @@ class App extends React.Component<any, IState>{
   pageView: any;
 
   getPageSrc = () => {
-    if(this.pageView) {
+    if (this.pageView) {
       return this.pageView.src
     }
     return '';
@@ -90,43 +90,43 @@ class App extends React.Component<any, IState>{
       getFieldDecorator
     } = this.props.form;
 
-    return ( <Layout style={{width:'100%', height: '100%'}}>
-                <Header style={{ position: 'absolute', zIndex: 1, width: '80%', height: '50px', padding:'10px' }}>
-                <Form  onSubmit={this.handleSubmit}>
-                  <Form.Item>
-                    {
-                      getFieldDecorator('url', {})(
-                        <Input prefix={<Icon type={this.state.isURLValid?"cloud":"ban"} style={{ color: 'rgba(0,0,0,.25)'}} />} placeholder="Url"/>
-                      )
-                    }
-                  </Form.Item>
-                  {/*<StalkerComponent 
+    return (<Layout style={{ width: '100%', height: '100%' }}>
+      <Header style={{ position: 'absolute', zIndex: 1, width: '80%', height: '50px', padding: '10px' }}>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Item>
+            {
+              getFieldDecorator('url', {})(
+                <Input prefix={<Icon type={this.state.isURLValid ? "cloud" : "ban"} style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Url" />
+              )
+            }
+          </Form.Item>
+          {/*<StalkerComponent 
                     interval={10000}
                     url={this.state.url} 
                   onChange={e => this.pageView.src = e} />*/}
-                </Form>
-                </Header>
+        </Form>
+      </Header>
 
 
-                <div className="card-container">
-                  <Tabs tabPosition='top' className='tabs-container' type="card">
-                    <TabPane className='main-container blending' forceRender key="1" 
-                      tab={
-                        <Slider 
-                          min={0} 
-                          max={1} 
-                          step={0.1} 
-                          onChange={e => this.setState({opacity: e})} 
-                          value={this.state.opacity} 
-                          style={{minWidth: 100}
-                        }/>}>
+      <div className="card-container">
+        <Tabs tabPosition='top' className='tabs-container' type="card">
+          <TabPane className='main-container blending' forceRender key="1"
+            tab={
+              <Slider
+                min={0}
+                max={1}
+                step={0.1}
+                onChange={e => this.setState({ opacity: e })}
+                value={this.state.opacity}
+                style={{ minWidth: 100 }
+                } />}>
 
-                          <TreeView style={{opacity: 1 - this.state.opacity}} url={this.state.url} />
-                          <webview ref={e => this.pageView = e} style={{width:'100%', height: '100%', opacity: this.state.opacity}} src='http://google.com'  />
-                    </TabPane>
-                  </Tabs>
-                </div>
-              </Layout>);
+            <TreeView style={{ opacity: 1 - this.state.opacity }} url={this.state.url} />
+            <webview ref={e => this.pageView = e} style={{ width: '100%', height: '100%', opacity: this.state.opacity }} src='http://google.com' />
+          </TabPane>
+        </Tabs>
+      </div>
+    </Layout>);
   }
 }
 
